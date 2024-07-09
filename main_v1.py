42cdb335c6ffa4b8fa742f898f0117919966236f
JS3322 <42951920+JS3322@users.noreply.github.com>
2024-07-09 16:54:12 +0900

    main_v1.py 업데이트

--- main_v1.py
+++ main_v1.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI, Request
+from src.analysis.application import AnalysisCleancodeApplication
+from src.ai.application import DeepLearningExampleApplication
+import uvicorn
 
 from s615_aop.service.Logging_Example import log_activity
 
 app = FastAPI()
+
+app.include_router(AnalysisCleancodeApplication.router, prefix="/v1/read")
+app.include_router(DeepLearningExampleApplication.router, prefix="/v1/execute")
 
 @app.get("/items/{item_id}")
 @log_activity
@@ -18,5 +24,4 @@ async def create_item(item: dict, request: Request):
 
 # The server should be run in a proper environment, not in this code cell
 if __name__ == "__main__":
-  import uvicorn
-  uvicorn.run(app, host="127.0.0.1", port=8000)
+  uvicorn.run(app, host="0.0.0.0", port=8000)
