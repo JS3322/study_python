8ed3234c8d607bcf9b593702ad44fc4a3f8fe091
JS3322 <42951920+JS3322@users.noreply.github.com>
2024-07-18 10:39:09 +0900

    create_model_application.py 업데이트

--- s711_data/application/create_model_application.py
+++ s711_data/application/create_model_application.py
@@ -1,10 +1,13 @@
 import logging
 import asyncio
 from src.s711_data.domain.service.calculate_process_service import execute_compute_process
+from src.s711_data.domain.service.calculate_process_service import generator_example
 
 def execute_create_model():
     try:
     	logging.info(" :: START :: execute_create_model")
     	asyncio.run(execute_compute_process())
+        for value in gen:
+            print(value)
     except Exception as e:
         raise Exception(e)
