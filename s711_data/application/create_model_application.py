c8caf97a61163ff60d1efd61986e98c02026fe4a
JS3322 <42951920+JS3322@users.noreply.github.com>
2024-07-18 10:42:40 +0900

    create_model_application.py 업데이트

--- s711_data/application/create_model_application.py
+++ s711_data/application/create_model_application.py
@@ -7,6 +7,9 @@ def execute_create_model():
     try:
     	logging.info(" :: START :: execute_create_model")
     	asyncio.run(execute_compute_process())
+        
+        # 한 번에 하나의 값을 반환하고, 함수 상태를 유지한 채로 중단
+        # iterator구현 또는 메모리 절약 위해 큰 데이터 스퀀스를 생성 용도
         for value in gen:
             print(value)
     except Exception as e:
