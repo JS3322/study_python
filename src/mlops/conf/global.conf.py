760dcaa84e24f549c4aa5cd96ed785579a42c163
JS3322 <42951920+JS3322@users.noreply.github.com>
2024-08-16 09:07:09 +0900

    global.conf.py 업데이트

---
+++ src/mlops/conf/global.conf.py
@@ -0,0 +1,18 @@
+EMAIL_CONSOLE_DOMAIN = ""
+EMAIL_SERVICE_NAME = "cleancode"
+
+DATABASE_AUTO_CREATE_INDEX = True
+DATABASE = {
+	"default" : {
+    	"db": "cleancode-service-list",
+        "host": "localhost",
+        "port": 27017,
+        "username": "",
+        "password: ""
+    }
+}
+
+ACTIVATE_SERVICE_LIST = [
+	"service-code-20240412",
+    "service-code-20251222"
+]
